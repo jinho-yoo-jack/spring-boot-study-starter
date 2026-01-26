46f1c116b8c46859319199d7c1f686a68e2505d4
jinho-yoo-jack <jhy7342@gmail.com>
2026-01-26 16:50:17 +0900

    MOD: Package Structur

--- src/main/java/com/study/myspringstudydiary/repository/StudyLogRepository.java
+++ src/main/java/com/study/myspringstudydiary/repository/StudyLogRepository.java
@@ -1,6 +1,6 @@
-package com.study.myspringstudydiary.domain.studylog.repository;
+package com.study.myspringstudydiary.repository;
 
-import com.study.myspringstudydiary.domain.studylog.entity.StudyLog;
+import com.study.myspringstudydiary.entity.StudyLog;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
