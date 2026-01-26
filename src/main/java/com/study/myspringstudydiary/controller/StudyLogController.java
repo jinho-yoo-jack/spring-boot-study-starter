d75fa82af1244f1bf18a11db6e8c7457eed4b554
jinho-yoo-jack <jhy7342@gmail.com>
2026-01-26 16:51:21 +0900

    MOD: Package Structur

--- src/main/java/com/study/myspringstudydiary/controller/StudyLogController.java
+++ src/main/java/com/study/myspringstudydiary/controller/StudyLogController.java
@@ -1,10 +1,10 @@
-package com.study.myspringstudydiary.domain.studylog.controller;
+package com.study.myspringstudydiary.controller;
 
-import com.study.myspringstudydiary.domain.studylog.dto.request.StudyLogCreateRequest;
-import com.study.myspringstudydiary.domain.studylog.dto.request.StudyLogUpdateRequest;
-import com.study.myspringstudydiary.domain.studylog.dto.response.StudyLogResponse;
-import com.study.myspringstudydiary.domain.studylog.dto.response.StudyLogDeleteResponse;
-import com.study.myspringstudydiary.domain.studylog.service.StudyLogService;
+import com.study.myspringstudydiary.dto.request.StudyLogCreateRequest;
+import com.study.myspringstudydiary.dto.request.StudyLogUpdateRequest;
+import com.study.myspringstudydiary.dto.response.StudyLogResponse;
+import com.study.myspringstudydiary.dto.response.StudyLogDeleteResponse;
+import com.study.myspringstudydiary.service.StudyLogService;
 import com.study.myspringstudydiary.global.common.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
